0ebd1cc8069d8cc090dc730376994615f1c7aa85
Saeeda <saeeda123@gmail.com>
2025-11-30 20:16:08 +0500

    first commit

--- JS/work.js
+++ JS/work.js
@@ -57,3 +57,67 @@ document.addEventListener("DOMContentLoaded", () => {
   // Default view = grid
   showView(workContent, workSection, gridBtn, listBtn, "grid");
 });
+
+
+document.querySelectorAll(".element").forEach(function (element) {
+
+  var rotate = 0;
+  var diffrot = 0;
+
+  element.addEventListener("mouseleave", function (dets) {
+
+    gsap.to(element.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  element.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - element.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+
+    gsap.to(element.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
+
+const wrapper = document.querySelector('.logos-wrapper');
+const logos = wrapper.innerHTML;
+
+// Duplicate logos for seamless loop
+wrapper.innerHTML += logos;
+
+const logosWidth = wrapper.scrollWidth / 2;
+
+gsap.to(wrapper, {
+  x: `-${logosWidth}px`,
+  duration: 20,
+  ease: "linear",
+  repeat: -1,
+  modifiers: {
+    x: gsap.utils.unitize(x => parseFloat(x) % logosWidth)
+  }
+});
+
+function updateFooterTime() {
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+    timeZone: 'Asia/Karachi'
+  };
+  const now = new Date();
+  const formattedTime = now.toLocaleTimeString('en-US', options);
+  document.getElementById('footer-time').textContent = formattedTime + " PKT";
+}
+
+updateFooterTime();
+setInterval(updateFooterTime, 1000); 
